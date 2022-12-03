96ffa4b88029a62810612d5f55b021b89f1f94a3
Mariana <you@example.comcl>
2022-12-03 19:46:00 +0000

    starting JS with a bug

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
-            // $(function () {
-            //     $('.next-step').click(function (event) {
-            //         event.preventDefault();
-            //         $(this).parents('.form-step').hide().next().show();
-            //     });
-            // });
-console.log('teste')
+  const form = document.querySelector('#form');
+  const fields = document.querySelectorAll('.required');
+  const span = document.querySelectorAll('.span-required');
+  const emailRefex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  function nameValidate(){
+      if(fields[0].value.lenght<3)
+      {
+         console.log('nome deve ter 3 caracteres');
+
+      }
+      else 
+      {
+        console.log('válido');
+      }
+       };
