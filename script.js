f67c647583dc7cdb0a37ea129625ae029dadcca8
Mariana <you@example.comcl>
2022-12-03 15:44:07 +0000

    creating API

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-            $(function () {
-                $('.next-step').click(function (event) {
-                    event.preventDefault();
-                    $(this).parents('.form-step').hide().next().show();
-                });
-            });
+            // $(function () {
+            //     $('.next-step').click(function (event) {
+            //         event.preventDefault();
+            //         $(this).parents('.form-step').hide().next().show();
+            //     });
+            // });
+console.log('teste')
