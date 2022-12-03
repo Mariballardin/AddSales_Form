c6d0c19c996fe01baad98d25bba60f2be1b5300e
Mariana <you@example.comcl>
2022-12-03 21:59:28 +0000

    another bug sos

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 
- /* const form = document.querySelector('#form');
-  const fields = document.querySelectorAll('.required');
-  const span = document.querySelectorAll('.span-required');
-  const emailRefex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  function nameValidate(){
-      if(fields[0].value.lenght<3)
-      {
-         console.log('nome deve ter 3 caracteres');
+const form = document.querySelector('#form');
+const fieldset = document.querySelectorAll('.required');
+const span = document.querySelectorAll('.span-required');
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-      }
-      else 
-      {
-        console.log('válido');
-      }
-       };
-*/
+
+
+//  form.addEventListener('submit', (event)=> {
+//    event.preventDefault();
+//    nameValidate();
+//    emailValidate();
+//   console.log('oi');
+
+
+// });
+
+form.addEventListener('submit', (event) => {
+  console.log('alo')
+});
+
+
+function setError(index)
+{
+ fieldset[index].style.border = '1px solid  #FF1010';
+ span[index].style.display ='block';
+
+
+}
+
+function removeError(index)
+{
+  fieldset[index].style.border = '';
+ span[index].style.display ='none';
+}
+function nameValidate(){
+    if(fieldset[0].value.length < 3)
+    {
+       setError(0);
+
+    }
+    else 
+    {
+      removeError(0);
+    }
+     }
+     
+function emailValidate()
+{
+  if (!emailRegex.test(fieldset[1].value))
+  {
+    setError(1);
+
+  }
+
+  else 
+  {
+    removeError(1);
+  }
+}
